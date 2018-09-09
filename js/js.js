1d4c9a20d9f08fa6d0365ed3cbd4236ec83f4483
Mr Mjir <39204944+MrMjir@users.noreply.github.com>
2018-09-08 22:30:47 -0500

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,11 +1,30 @@
-/*===============
-Functions when the
-DOM loads
-  ===============*/
+/*===================================
+  Global Variables
+  ===================================*/
+var $window = $(window);
+var BANNER_HEIGHT;
+
+/*===================================
+  When DOM loads function
+  ===================================*/
 $(function() {
-	setWallpaperPic();
+	setBannerPic();
+	//Resize functions
+	$window.resize(function resize(){
+		BANNER_HEIGHT = $window.width() * 0.25;
+		setBannerSize();
+	});
 });
 
-function setWallpaperPic() {
+
+/*===================================
+  Wallpaper Functions
+  ===================================*/
+//Set the wallpaper of a banner div
+function setBannerPic() {
 	$(".banner").css({"background-image" : "url(" + $(".banner").attr("id") + ")"});
+}
+
+function setBannerSize() {
+	$(".banner").css({"height" : String(BANNER_HEIGHT) + "px"});
 }
